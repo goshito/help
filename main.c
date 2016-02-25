1276849d03001573fdcb5bf27daac5fdb89f08dd
Georgi <Georgi@Georgi-PC>
2016-02-25 15:00:12 +0200

    read backspace \b

---
+++ main.c
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: Georgi
+ *
+ * Created on 25 Февруари 2016, 13:53
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    char c;
+    
+    c = getchar();
+    printf("%c", c);
+
+    return (EXIT_SUCCESS);
+}
+
