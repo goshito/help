3e0f8ba8762bb3597f26caebed963247a06cb92d
Georgi <Georgi@Georgi-PC>
2016-02-25 22:53:22 +0200

    termios

--- main.c
+++ main.c
@@ -8,21 +8,61 @@
  * File:   main.c
  * Author: Georgi
  *
- * Created on 25 Февруари 2016, 13:53
+ * Created on 25 Февруари 2016, 21:39
  */
 
+#include <termios.h>
 #include <stdio.h>
-#include <stdlib.h>
 
-/*
- * 
- */
-int main(int argc, char** argv) {
-    char c;
-    
-    c = getchar();
-    printf("%c", c);
+static struct termios old, new;
 
-    return (EXIT_SUCCESS);
+/* Initialize new terminal i/o settings */
+void initTermios(int echo) 
+{
+  tcgetattr(0, &old); /* grab old terminal i/o settings */
+  new = old; /* make new settings same as old settings */
+  new.c_lflag &= ~ICANON; /* disable buffered i/o */
+  new.c_lflag &= echo ? ECHO : ~ECHO; /* set echo mode */
+  tcsetattr(0, TCSANOW, &new); /* use these new terminal i/o settings now */
 }
+
+/* Restore old terminal i/o settings */
+void resetTermios(void) 
+{
+  tcsetattr(0, TCSANOW, &old);
+}
+
+/* Read 1 character - echo defines echo mode */
+char getch_(int echo) 
+{
+  char ch;
+  initTermios(echo);
+  ch = getchar();
+  resetTermios();
+  return ch;
+}
+
+/* Read 1 character without echo */
+char getch(void) 
+{
+  return getch_(0);
+}
+
+/* Read 1 character with echo */
+char getche(void) 
+{
+  return getch_(1);
+}
+
+/* Let's test it out */
+int main(void) {
+  char c;
+  printf("(getche example) please type a letter: ");
+  c = getche();
+  printf("\nYou typed: %c\n", c);
+  printf("(getch example) please type a letter...");
+  c = getch();
+  printf("\nYou typed: %c\n", c);
+  return 0;
+} 
 
