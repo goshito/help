02d32d2c137c3826b4cfac4a19492f80510697f5
joring <joring@edubuntu>
2016-02-26 15:32:11 +0200

    netbeans pod edubuntu 3:31pm

---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: joring
+ *
+ * Created on February 26, 2016, 3:29 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
